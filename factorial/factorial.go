3edfa01dc82187be40e3bf5baf529a718462728d
AlperenTurkmen <alperen.turkmen@gmail.com>
2023-03-03 13:15:57 +0000

    first iterative commit

--- factorial/factorial.go
+++ factorial/factorial.go
@@ -1,9 +1,9 @@
 package factorial
 
 func Factorial(n int) int {
-	if n == 0 {
-		return 1
-	} else {
-		return n * Factorial(n-1)
+	result := 1
+	for i := 1; i <= n; i++ {
+		result *= i
 	}
+	return result
 }
